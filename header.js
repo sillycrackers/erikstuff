da1a5b2bb8a4b7497976cba24ae5c383ebc232e6
Erik Westerveld <westervelderik@gmail.com>
2025-04-21 11:06:37 -0400

    stuff

---
+++ header.js
@@ -0,0 +1,13 @@
+// This is my first javascript code
+console.log('Hello World');
+
+fetch('./header.html').then(function (response){
+    if (response.ok) {
+		return response.text();
+	}
+	throw response;
+}).then(function (text) {
+    let body = document.querySelector('body');
+    body.innerHTML = text;
+	
+});
